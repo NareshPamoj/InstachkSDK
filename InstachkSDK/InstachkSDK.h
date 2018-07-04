922998c9c6fa2fec83e6d3f5dc5efdcb80bde17f
Amit Chatterjee <amit.chatterjee@gmail.com>
2018-07-04 09:33:31 +0530

    Initial Commit

---
+++ InstachkSDK/InstachkSDK.h
@@ -0,0 +1,19 @@
+//
+//  InstachkSDK.h
+//  InstachkSDK
+//
+//  Created by Amit Chatterjee on 4/7/18.
+//  Copyright © 2018 Amit Chatterjee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for InstachkSDK.
+FOUNDATION_EXPORT double InstachkSDKVersionNumber;
+
+//! Project version string for InstachkSDK.
+FOUNDATION_EXPORT const unsigned char InstachkSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InstachkSDK/PublicHeader.h>
+
+
